58373a9399887210da3ad04b0539eb51b3307f1b
Bernardo <Bernardo@INF2-PC9>
2017-03-22 21:24:01 +0100

    Primera version en php

---
+++ Boletin 1/boletin4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       $base=4;
+        $exp=8;
+        $elevada= pow($base, $exp);
+        $raiz8=sqrt(8);
+        $raiz4=sqrt(4);
+        echo "El resultado es $elevada";
+        echo "<br>";
+        echo "La raiz de 8 es $raiz8";
+        echo "<br>";
+        echo "La raiz de 4 es $raiz4";
+        ?>
+    </body>
+</html>
