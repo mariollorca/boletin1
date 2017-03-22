a613743aa8aa318406b3127f7006aca2d32549d2
Bernardo <Bernardo@INF2-PC9>
2017-03-22 21:11:50 +0100

    Primera version en php

---
+++ Boletin 1/ejercicio2.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+ <html>
+ <head></head>
+ <body>
+ <?php
+  $primera=4;
+        $segunda=8;
+        $suma=$primera+$segunda;
+        $producto=$primera*$segunda;
+        print "La suma es $suma";
+        echo "<br>";
+        print "El producto es $producto";
+ ?>
+ </body>
+</html>
+
