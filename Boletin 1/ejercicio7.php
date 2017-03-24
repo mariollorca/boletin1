39d7fcf8b227937407317855dc450d142d63b62e
mariollorca <mariollorca@INF2-PC08>
2017-03-24 20:55:54 +0100

    En el missatge definim la versio: Primera versio en php

---
+++ Boletin 1/ejercicio7.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $numero=70;
+        
+        print"Introduce numero: $numero";
+        echo "<br>";
+        
+        if($numero%2==0){
+            echo "El valor es par";
+        }
+        else{
+            echo "El valor es impar";
+        }
+        ?>
+    </body>
+</html>
