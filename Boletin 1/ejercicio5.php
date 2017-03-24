6e58bc5d841e4df6c6372adcec0bf6eeb7bd9c4e
mariollorca <mariollorca@INF2-PC08>
2017-03-24 20:55:30 +0100

    En el missatge definim la versio: Primera versio en php

---
+++ Boletin 1/ejercicio5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       $cadena1="Hola a todo el mundo";
+       $cadena2="Mi nombre es nombre y apellidos del alumno";
+       $cadena3=$cadena1."".$cadena2;
+       echo "$cadena3";
+        ?>
+    </body>
+</html>
