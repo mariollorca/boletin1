d69f4b20037179573500f049aba7576f62aa891e
mariollorca <mariollorca@INF2-PC08>
2017-03-24 20:54:32 +0100

    En el missatge definim la versio: Primera versio en php

---
+++ Boletin 1/ejercicio3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $primera=10;
+        $segunda=2;
+        $resta=$primera-$segunda;
+        $division=$primera/$segunda;
+        print "La diferencia es $resta";
+        echo "<br>";
+        print "La division es $division";
+        
+        ?>
+    </body>
+</html>
