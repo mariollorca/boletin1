faba0de870ae546746c80b8ea8e0dcfc662a43eb
Bernardo <Bernardo@INF2-PC9>
2017-03-29 19:57:25 +0200

    Primera version en php

---
+++ Boletin 1/ejercicio6.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $numero1=rand(1,50);
+        $numero2=rand(1,50);
+        $numero3=rand(1,50);
+        $numero4=rand(1,50);
+        $numero5=rand(1,50);
+        $estrella1=rand(1,9);
+        $estrella2=rand(1,9);
+        echo "El valor sorteado es $numero1 $numero2 $numero3 $numero4 $numero5";
+        echo "<br>";
+        echo "El valor sorteado es $estrella1 $estrella2";
+      
+        ?>
+    </body>
+</html>
